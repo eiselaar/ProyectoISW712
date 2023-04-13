171fd9b5ec1e54f01ae9494ddb4addeaf9370695
Emily Arias <100312848+eiselaar@users.noreply.github.com>
2023-04-12 20:06:00 -0600

    ProyectoISW712

---
+++ ProyectoISW712/ProyectoISW712/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoISW712.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
